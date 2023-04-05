83f047c26b3d6de44d919e322e447432f927c33f
Chad Tarpey <chadtarpey@gmail.com>
2023-04-05 17:22:38 -0500

    added endoint for movie details.html page

--- js/script.js
+++ js/script.js
@@ -63,6 +63,57 @@ async function displayPopularShows() {
         document.querySelector('#popular-shows').appendChild(div);
     })
 }
+
+// display movie details
+async function displayMovieDetails() {
+    const movieId = window.location.search.split("=")[1]
+    const movie = await fetchApiData(`movie/${movieId}`)
+    const { title, poster_path, release_date, vote_average, overview, homepage, budget, revenue, runtime, status } = movie;
+    const div = document.createElement('div');
+    div.innerHTML = `
+      <div class="details-top">
+          <div>
+            ${poster_path
+            ? `
+            <img src="https://image.tmdb.org/t/p/w500${poster_path}" alt=${title} class="card-img-top" />
+                
+                `: `
+            <img src="../images/no-image.jpg" alt="no images" class="card-img-top" />
+                
+                `
+        }
+          </div>
+          <div>
+            <h2>${title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${vote_average.toFixed(1)}/ 10
+            </p>
+            <p class="text-muted">Release Date: ${release_date}</p>
+            <p>
+             ${overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+             ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href=${homepage} target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span>  ${addCommasToNumber(budget)}</li>
+            <li><span class="text-secondary">Revenue:</span> ${addCommasToNumber(revenue)}</li>
+            <li><span class="text-secondary">Runtime:</span> ${runtime} minutes</li>
+            <li><span class="text-secondary">Status:</span> ${status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${movie.production_companies.map((company) => `<span>${company.name}</span>`).join(', ')}</div>
+        </div>
+`;
+    document.querySelector('#movie-details').appendChild(div);
+}
 // fetch data from TMDB API
 async function fetchApiData(endpoint) {
     const API_KEY = '81d8e294631eb763043051e297c1fb2d';
@@ -97,6 +148,11 @@ function highlightActiveLink() {
     });
 }
 
+
+function addCommasToNumber(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 // init app
 function init() {
     switch (global.currentPage)
@@ -108,8 +164,8 @@ function init() {
         case '/shows.html':
             displayPopularShows()
             break;
-        case '/movie-detail.html':
-            console.log('Movie detail');
+        case '/movie-details.html':
+            displayMovieDetails()
             break;
         case 'tv-detail.html':
             console.log('TV detail');
